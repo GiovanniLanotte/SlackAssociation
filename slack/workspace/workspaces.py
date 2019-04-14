25a4e03e36b86f7536f879f7a94846f9dc30b34e
Giovanni Lanotte <giovanni.lanotte@hotmail.it>
2019-04-14 13:08:26 +0200

    Aggiunta del metodo 'get_csv_channel_organization' della classe 'Workspaces'.

--- slack/workspace/workspaces.py
+++ slack/workspace/workspaces.py
@@ -4,6 +4,8 @@
 Created on Wed Sep 19 13:51:07 2018
 @author: giovanni
 """
+import csv
+from slack.element_slack.channels import Channels
 
 
 class Workspaces:
@@ -20,3 +22,16 @@ class Workspaces:
 
     def get_names_channel(self):
         return self._channels.keys()
+
+    def get_csv_channel_organization(self):
+        name_file = 'channels ' + self._name_workspace + '.csv'
+        file = open(name_file, 'wt')
+        try:
+            writer: csv.DictWriter = csv.writer(file)
+            writer.writerow(
+                ('name', "# users"))
+            for key in self._channels:
+                channel:Channels = self._channels.get(key)
+                writer.writerow((channel.get_name_channel(), len(channel.get_users())))
+        finally:
+            file.close()
