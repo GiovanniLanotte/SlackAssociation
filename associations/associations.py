1d2495e7dcc9601f84ac9b19d5f562072c406285
Giovanni Lanotte <giovanni.lanotte@hotmail.it>
2019-04-14 19:26:43 +0200

    Modifica del costruttore della classe Associations.

--- associations/associations.py
+++ associations/associations.py
@@ -1,15 +1,16 @@
 
 class Associations:
 
-    def __init__(self, repository, channel, name_organization, name_file_issue, name_file_comments_issue,
+    def __init__(self, directory, repository, channel, name_organization, name_file_issue, name_file_comments_issue,
                  name_file_pull_request, name_file_comments_pull_request):
         self._repository = repository
         self._channel = channel
         self._name_organization = name_organization
-        self._name_file_issue = name_file_issue
-        self._name_file_comments_issue = name_file_comments_issue
-        self._name_file_pull_request = name_file_pull_request
-        self._name_file_comments_pull_request = name_file_comments_pull_request
+        self._name_file_issue = directory + '/' + self._name_organization + '/' + name_file_issue
+        self._name_file_comments_issue = directory + '/' + self._name_organization + '/' + name_file_comments_issue
+        self._name_file_pull_request = directory + '/' + self._name_organization + '/' + name_file_pull_request
+        self._name_file_comments_pull_request = \
+            directory + '/' + self._name_organization + '/' + name_file_comments_pull_request
 
     def get_repository(self):
         return self._repository
