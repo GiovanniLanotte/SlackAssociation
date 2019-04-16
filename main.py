d03af24e6b4e695c58f55b8b72e203dd3102f49d
Giovanni Lanotte <giovanni.lanotte@hotmail.it>
2019-04-16 14:41:47 +0200

    Modifica del metodo main.

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ def main():
     tokens = read_token_file(sys.argv[3])
     containers: Containers = Containers()
     containers.add_association(sys.argv[1], sys.argv[2], tokens)
-    containers.correspondence_channel_to_archive()
-    containers.get_association_channel_repository(sys.argv[1])
-    containers.get_len_message_for_channel()
+    containers.correspondence_channel_to_archive(sys.argv[1])
+    containers.get_len_message_for_channel(sys.argv[1])
     containers.messages_for_channel()
     containers.messages_slack_url_github()
     containers.pull_request_on_message_slack()
     containers.messages_for_channel()
+
 
 if __name__ == '__main__':
     main()
